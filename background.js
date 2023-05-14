aed060a0402186bdd32ab58d37a1a39bd62e0231
kamirov <andrei.khramtsov@gmail.com>
2023-05-14 12:34:40 -0400

    Add Russia + update schedule

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ const icons = {
   france: 'icons/france.png',
   spain: 'icons/spain.png',
   india: 'icons/india.png',
+  russia: 'icons/russia.png',
   default: 'icons/default.png',
 };
 
@@ -12,14 +13,21 @@ function getIcon() {
   const hour = now.getHours();
   const day = now.getDay();
 
-  if (day === 1 || day === 3 || day === 5) { // Monday, Wednesday, Friday
-    if (hour >= 13 && hour < 16) return icons.france;
-    if (hour >= 16 && hour < 20) return icons.spain;
-    if (hour >= 20 && hour < 22) return icons.india;
-  } else if (day === 2 || day === 4) { // Tuesday, Thursday
-    if (hour >= 13 && hour < 16) return icons.spain;
-    if (hour >= 16 && hour < 20) return icons.france;
-    if (hour >= 20 && hour < 22) return icons.india;
+  if (day === 1 || day === 3 || day === 5 || day === 7) { // Monday, Wednesday, Friday, Sunday
+    if (hour >= 8 && hour < 11) return icons.france;
+    if (hour >= 11 && hour < 14) return icons.spain;
+    if (hour >= 14 && hour < 15) return icons.russia;
+    if (hour >= 15 && hour < 17) return icons.india;
+    if (hour >= 17 && hour < 20) return icons.france;
+    if (hour >= 20 && hour < 23) return icons.spain;
+  } else if (day === 2 || day === 4 || day === 6) { // Tuesday, Thursday, Saturday
+    if (hour >= 8 && hour < 11) return icons.spain;
+    if (hour >= 11 && hour < 14) return icons.france;
+    if (hour >= 14 && hour < 15) return icons.spain;
+    if (hour >= 15 && hour < 17) return icons.india;
+    if (hour >= 17 && hour < 20) return icons.spain;
+    if (hour >= 20 && hour < 23) return icons.france;
+
   }
 
   return icons.default;
@@ -28,7 +36,7 @@ function getIcon() {
 // This function updates the browser action icon
 function updateIcon() {
   const icon = getIcon();
-  chrome.browserAction.setIcon({ path: icon });
+  chrome.action.setIcon({ path: icon });
 }
 
 // Update the icon when the extension is installed
